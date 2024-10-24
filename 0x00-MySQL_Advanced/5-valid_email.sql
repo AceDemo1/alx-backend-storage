51c683194e5812a78272a3f11bf88d7e976061e7
AceDemo1 <lawal641@gmail.com>
2024-10-24 23:12:38 +0000

    create trigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,8 @@ CREATE TRIGGER valid
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-	IF NEW.email = OLD.email THEN
-		SET NEW.valid_email = 1;
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
 	END IF;
 END /
 DELIMITER ;
