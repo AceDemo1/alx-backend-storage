01f3d12b9f69adb76c8f964e95f7bc02ed42413b
AceDemo1 <lawal641@gmail.com>
2024-10-25 23:39:30 +0000

    create procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
 	SELECT AVG(score) INTO avg_score FROM corrections
-	WHERE user_id = user_id;
+	WHERE correction.user_id = user_id;
 	
 	UPDATE users
 	SET average_score = IFNULL(avg_score, 0)
