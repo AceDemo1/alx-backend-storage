a41739ef982a338e3114ffde57f720c8b49d6f77
AceDemo1 <lawal641@gmail.com>
2024-10-27 15:57:41 -0700

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""insert new docs"""
+"""update new docs"""
 
 def update_topics(mongo_collection, name, topics):
     """define func"""
-    mongo_collection.update_one({ name: name }, { $set { 'topics': topics }})
+    mongo_collection.update({ 'name': name }, { '$set': { 'topics': topics }})
