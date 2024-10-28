bb900ea17c0b274ee680982e07be1fbe9611b625
AceDemo1 <lawal641@gmail.com>
2024-10-27 20:36:47 -0700

    log_bd

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     get_path = coll.count_documents({ 'method': 'GET', 'path': '/status'})
     method_count = {'GET': get, 'POST': post, 'PUT': put, 'PATCH': patch, 'DELETE': delete}
     for method, count in method_count.items():
-        print(f'\tMethod {method}: {count}')
+        print(f'\tmethod {method}: {count}')
     print(f'{get_path} status check')
