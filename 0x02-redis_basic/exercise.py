155f534f0d6fb116c1abc81a8c48731851cc16ea
AceDemo1 <lawal641@gmail.com>
2024-10-30 15:37:15 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,7 +9,7 @@ class Cache:
     def __init__(self):
         """initializes"""
         self._redis = redis.Redis()
-        self._redis = flushdb()
+        self._redis.flushdb()
 
     def store(self, data: Any) -> str:
         """stores"""
