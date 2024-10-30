8368308da1931cdb7823cb29cffba4e75096db57
AceDemo1 <lawal641@gmail.com>
2024-10-30 22:16:32 +0000

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[bytes], Any] = None) -> Any:
+        """get value"""
+        value = self._redis.get(key)
+        return fn(value) if fn else value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """get_str"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """get_int"""
+        return self.get(key, fn=int)
