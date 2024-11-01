777ccf0e2e1e441dd4c47d1c346611eb3eea6f37
AceDemo1 <lawal641@gmail.com>
2024-11-01 01:36:15 +0000

    redis

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import requests
+import redis
+from typing import Callable, Any
+import functools
+
+r = redis.Redis()
+def coun(func: Callable) -> Callable:
+    """count response"""
+    @functools.wraps(func)
+    def wrapper(url: str) -> Any:
+        """wrapper func"""
+        count_key = f'count:{url}'
+        content_key = f'content:{url}'
+        r.incr(count_key)
+        cached = r.get(content_key)
+        if cached:
+            return cached.decode('utf-8')
+        content = func(url)
+        r.setex(content_key, 10, content)
+        return content
+    return wrapper
+
+@count
+def get_page(url: str) -> str:
+    """obtain HTML content and returns it"""
+    return requests.get(url).text
